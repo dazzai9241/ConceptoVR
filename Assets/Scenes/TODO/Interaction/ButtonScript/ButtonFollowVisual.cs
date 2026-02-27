994bfb804c7d62bb7484a669e68f035c1e3f7b13
Fantasya63 <baloaloajansen.school@gmail.com>
2026-02-28 00:01:28 +0800

    Fixed button bug

--- Assets/Scenes/TODO/Interaction/ButtonScript/ButtonFollowVisual.cs
+++ Assets/Scenes/TODO/Interaction/ButtonScript/ButtonFollowVisual.cs
@@ -6,8 +6,7 @@ public class ButtonFollowVisual : MonoBehaviour
 {
     [Header("References")]
     public Transform visualTarget;
-    public Vector3 localAxis = Vector3.up;
-
+    
     [Header("Settings")]
     public float resetSpeed = 5;
     public float followAngleThreshold = 45;
@@ -18,22 +17,27 @@ public class ButtonFollowVisual : MonoBehaviour
     private Transform unpressedHeight;
     [SerializeField]
     private Transform pressedHeight;
+    
+    
 
     [Header("Events")]
     public UnityEvent OnPressed;
     public UnityEvent OnReleased;
 
-    private Vector3 offset;
-    private Vector3 initialLocalPos;
     private Transform pokeAttachTransform;
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable;
-    private bool isFollowing = false;
+    private bool isPoked = false;
 
-    private bool isValid = true;
     
     private bool isPressed = false;
-
     private bool wasIsPressed = false;
+
+    // Quaternion to rotate from model to buttonDir space
+    private Quaternion buttonUPQuat = Quaternion.identity;
+    
+    // The direction from the origin to the unpressed height marker
+    private Vector3 m_VectorToSurface = Vector3.zero;
+    private Vector3 m_ButtonUpDirection = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -42,11 +46,14 @@ public class ButtonFollowVisual : MonoBehaviour
         Debug.Assert(unpressedHeight != null);
         Debug.Assert(pressedHeight != null);
 
-        initialLocalPos = visualTarget.localPosition;
-
         interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
         interactable.hoverEntered.AddListener(OnPoke);
-        interactable.hoverExited.AddListener(Reset);
+        interactable.hoverExited.AddListener(PokeExited);
+
+
+        m_VectorToSurface = unpressedHeight.transform.localPosition - transform.localPosition;
+        m_ButtonUpDirection = m_VectorToSurface.normalized;
+        buttonUPQuat = Quaternion.FromToRotation(m_ButtonUpDirection, Vector3.up);
     }
 
     public void OnPoke(BaseInteractionEventArgs hover)
@@ -55,47 +62,46 @@ public class ButtonFollowVisual : MonoBehaviour
         if(hover.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)
         {
             UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor interactor = (UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)hover.interactorObject;
-
-            isFollowing = true;
+            isPoked = true;
          
             pokeAttachTransform = interactor.attachTransform;
-            offset = visualTarget.InverseTransformPoint(pokeAttachTransform.position) - visualTarget.localPosition;
 
-            // Check if finger is above the pressed marker
-            if (Vector3.Dot(Vector3.up, visualTarget.InverseTransformDirection(pokeAttachTransform.position - pressedHeight.position)) > 0.0)
-            {
-                isValid = true;
-                isFollowing = true;
-            }
-            else
-            {
-                isValid = false;
-                isFollowing = false;
-            }
         }
     }
 
-    public void Reset(BaseInteractionEventArgs hover)
+    private Vector3 WorldPosToButtonPos(Vector3 point)
+    {
+        return LocalPosToButtonPos(transform.InverseTransformPoint(point));
+    }
+
+    private Vector3 LocalPosToButtonPos(Vector3 point)
+    {
+        return buttonUPQuat * point;
+    }
+
+
+
+    public void PokeExited(BaseInteractionEventArgs hover)
     {
         if (hover.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)
         {
-            isFollowing = false;
-            isValid = true;
+            isPoked = false;
+          
         }
     }
 
 
-    public void CheckForPressed(Vector3 localPokePos, Vector3 localPressPos)
+    public void CheckForPressed(Vector3 buttonPokePos, Vector3 buttonPressPos)
     {
-        isPressed = localPokePos.y <= localPressPos.y;
-
-        
+        isPressed = buttonPokePos.y <= buttonPressPos.y;
         if (isPressed)
         {
             if (!wasIsPressed)
             {
                 if (OnPressed != null)
                     OnPressed.Invoke();
+
+                Debug.Log("Pressed");
             }
         }
         else
@@ -104,38 +110,82 @@ public class ButtonFollowVisual : MonoBehaviour
             {
                 if (OnReleased != null)
                     OnReleased.Invoke();
+                Debug.Log("Released");
             }
         }
         wasIsPressed = isPressed;
     }
 
+    // Takes the poke, pressed, and unpressed positions in the button space
+    bool IsValid(Vector3 buttonPoke, Vector3 ButtonUnpressed)
+    {
+        Vector3 releasedPosButton = WorldPosToButtonPos(unpressedHeight.position);
+        Vector3 pokePosButton = WorldPosToButtonPos(pokeAttachTransform.position);
+
+        return (pokePosButton.y <= releasedPosButton.y);
+    }    
+
     // Update is called once per frame
     void Update()
     {
-
-        if (isFollowing)
+        if (isPoked)
         {
-            if (!isValid)
+            Vector3 buttonPokePos = WorldPosToButtonPos(pokeAttachTransform.position);
+            Vector3 buttonUnpressedPos = WorldPosToButtonPos(unpressedHeight.position);
+
+
+            if (!IsValid(buttonPokePos, buttonUnpressedPos))
                 return;
 
-            Vector3 localPokePos = visualTarget.InverseTransformPoint(pokeAttachTransform.position);
-            Vector3 localPressedPos = visualTarget.InverseTransformPoint(pressedHeight.position);
+            
+            Vector3 buttonPressedPos = WorldPosToButtonPos(pressedHeight.position);
+            Vector3 buttonVectorToSurface = LocalPosToButtonPos(m_VectorToSurface);
 
-            CheckForPressed(localPokePos, localPressedPos);
-            
-            // Only allow the visual transform to move between inital pos, and pressed pos
-            Vector3 pos = localPokePos;
-            if (pos.y < localPressedPos.y)
-                pos.y = localPressedPos.y;
-            
-            
-            //Vector3 localTargetPosition = visualTarget.InverseTransformPoint(pos - offset);
-            Vector3 constrainedLocalTargetPosition = Vector3.Project(pos - offset, localAxis);
-            visualTarget.position = visualTarget.TransformPoint(constrainedLocalTargetPosition);
+            CheckForPressed(buttonPokePos, buttonPressedPos);
+           
+
+            Vector3 visualLocalPos =  Vector3.Project(buttonPokePos - buttonVectorToSurface, Vector3.up);
+            float maxButtonY = buttonUnpressedPos.y - buttonVectorToSurface.y;
+            if (visualLocalPos.y > maxButtonY)
+                visualLocalPos.y = maxButtonY;
+
+            float minButtonY = buttonPressedPos.y - buttonVectorToSurface.y;
+            if (visualLocalPos.y < minButtonY)
+                visualLocalPos.y = minButtonY;
+
+            visualLocalPos = Quaternion.Inverse(buttonUPQuat) * visualLocalPos;
+           
+            visualTarget.transform.position = transform.TransformPoint(visualLocalPos);  
+
         }
         else
         {
-            visualTarget.localPosition = Vector3.Lerp(visualTarget.localPosition, initialLocalPos, Time.deltaTime * resetSpeed);
+            visualTarget.localPosition = Vector3.Lerp(visualTarget.localPosition, transform.localPosition, Time.deltaTime * resetSpeed);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isPoked)
+        {
+            float sphereRad = 0.001f;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(pokeAttachTransform.position, sphereRad);
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(unpressedHeight.position, sphereRad);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(pressedHeight.position, sphereRad);
+
+            Ray buttonUP = new Ray(pressedHeight.position, transform.TransformDirection(m_ButtonUpDirection));
+            Gizmos.DrawRay(buttonUP);
+
+            Ray TestRay = new Ray(pressedHeight.position, transform.TransformDirection(Quaternion.Inverse(buttonUPQuat) * Vector3.up));
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(TestRay);
+
         }
     }
 }
