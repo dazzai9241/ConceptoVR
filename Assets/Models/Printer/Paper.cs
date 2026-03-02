8e6ba355c0df405294bfcec68b341832b1a76384
Fantasya63 <baloaloajansen.school@gmail.com>
2026-03-02 23:41:01 +0800

    Added paper types

--- Assets/Models/Printer/Paper.cs
+++ Assets/Models/Printer/Paper.cs
@@ -1,7 +1,54 @@
 using UnityEngine;
 
-[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshRenderer))]
 public class Paper : MonoBehaviour
 {
+    public enum PAPER_TYPE
+    {
+        Data = 0,
+        Hashkey,
+    };
+
+    public PAPER_TYPE PaperType
+    {
+        get { return paperType; }
+        set
+        {
+            if (paperType != value)
+            {
+                paperType = value;
+                UpdateMaterial();
+            }
+        }
+    }
+
+    [SerializeField] private Material hashkeyMat;
+    [SerializeField] private Material dataMat;
+
     public string data = "$$$";
+
+    private MeshRenderer meshRenderer;
+    private PAPER_TYPE paperType = PAPER_TYPE.Data;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        UpdateMaterial();
+    }
+
+    private void UpdateMaterial()
+    {
+        if (meshRenderer == null) return;
+
+        switch (paperType)
+        {
+            case PAPER_TYPE.Data:
+                meshRenderer.material = dataMat;
+                break;
+
+            case PAPER_TYPE.Hashkey:
+                meshRenderer.material = hashkeyMat;
+                break;
+        }
+    }
 }
